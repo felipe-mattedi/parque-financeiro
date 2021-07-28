9942c183660b18c516d067cc75c853ad85992043
Felipe Mattedi <felipe.mattedi@terra.com.br>
2021-07-28 20:14:18 -0300

    Altera porta

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/health', (req, res) => {
+  res.status(200)
   res.send('OK')
 })
 
 app.listen(port, () => {
-  console.log(`Sistema inicializado em http://localhost:${port}`)
+  console.log(`Sistema inicializado em ${port}`)
 })
