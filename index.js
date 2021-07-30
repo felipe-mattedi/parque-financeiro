aaf2638ae81184615cf699c3f1f2f68af804e9b5
Felipe Mattedi <felipe.mattedi@terra.com.br>
2021-07-29 23:35:59 -0300

    teste db

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express')
 const app = express()
+var AWS = require("aws-sdk");
 const port =  process.env.PORT || 3000
+
+AWS.config.update({
+  region: "us-east-2",
+});
+
+var docClient = new AWS.DynamoDB.DocumentClient();
+var table = "caixa-financeiro";
+
+var params = {
+  TableName:table,
+  Item:{
+      "id_lancamento": "00001",
+      "title": "valor"
+  }
+};
+
 
 const router = express.Router();
 
 router.get('/consulta', (req, res) => {
+
+  console.log("Adding a new item...");
+  docClient.put(params, function(err, data) {
+    if (err) {
+        console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Added item:", JSON.stringify(data, null, 2));
+    }
+});
+
+
   res.status(200).send({saldo: 23,
   observacao: process.env.PORT})
 })
