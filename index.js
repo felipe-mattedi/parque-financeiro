7f12dc6e24f49c4e0a3590acc6b55e820a35aafc
Felipe Mattedi <felipe.mattedi@terra.com.br>
2021-08-07 14:40:19 -0300

    ajustes

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 import { inserelancamento, consultalancamentos, inicializaaws } from './dynamo.js'
 import uuid from 'uuid/v4.js';
-import express  from 'express'
+import express from 'express'
 import bodyParser from 'body-parser'
-import  logger  from './logger.js'
-import { inicializalogger, inicializacatcher }from './middleware.js'
+import logger from './logger.js'
+import { inicializalogger, inicializacatcher } from './middleware.js'
 import axios from 'axios'
 import { gettoken, autenticador, deletetoken } from './auth.js'
 const app = express()
-const port =  process.env.PORT || 3000
+const port = process.env.PORT || 3000
 
 const router = express.Router();
 
 router.post('/lancamento', async (req, res) => {
-  if(!req.body.descricao || !req.body.valor){
-    res.status(400).send({status: "NOK", motivo: "Campo 'desricao' e 'valor' são obrigatórios para lancamentos"})
+  if (!req.body.descricao || !req.body.valor) {
+    res.status(400).send({ status: "NOK", motivo: "Campo 'desricao' e 'valor' são obrigatórios para lancamentos" })
     return
   }
-  try{
+  try {
     let uuidv4 = uuid()
     await inserelancamento(uuidv4, req.body.valor, req.body.descricao)
-    res.status(200).send({status: 'OK', id: uuidv4})
+    res.status(200).send({ status: 'OK', id: uuidv4 })
   }
-  catch(resposta){
+  catch (resposta) {
     res.status(resposta.statusCode || 500)
     next(resposta)
   }
 })
 
 router.get('/consulta', async (req, res, next) => {
-  try{
+  try {
     let consulta = await consultalancamentos()
     let saldo = 0
-    for (const k in consulta){
+    for (const k in consulta) {
       saldo = saldo + parseFloat(consulta[k].valor.N)
     }
-    consulta.push({saldo_total: saldo}) 
+    consulta.push({ saldo_total: saldo })
     res.status(200).send(consulta)
   }
-  catch(resposta){
+  catch (resposta) {
     res.status(resposta.statusCode || 500)
     next(resposta)
   }
@@ -47,20 +47,20 @@ router.get('/consulta', async (req, res, next) => {
 router.get('/balanco', async (req, res, next) => {
   let creditoinicial = 0
   let valor = 0
-  try{
-    let resposta = await axios.get('http://parque-ingressos.herokuapp.com/exportacompras',{
+  try {
+    let resposta = await axios.get('http://parque-ingressos.herokuapp.com/exportacompras', {
       headers: {
         Authorization: await gettoken()
       }
-     })
-    for(const k in resposta.data){
+    })
+    for (const k in resposta.data) {
       creditoinicial = creditoinicial + resposta.data[k].creditoInicial
-      valor = valor +  resposta.data[k].valor
+      valor = valor + resposta.data[k].valor
     }
     let total = creditoinicial + valor
-    res.status(200).send({creditoinicial: creditoinicial, valor: valor, total: total})
+    res.status(200).send({ creditoinicial: creditoinicial, valor: valor, total: total })
   }
-  catch(resposta){
+  catch (resposta) {
     res.status(resposta.response.status || 500)
     next(resposta)
   }
@@ -73,8 +73,8 @@ router.delete('/token', async (req, res) => {
 )
 
 router.get('/', async (req, res) => {
-    res.status(200).send()
-  }
+  res.status(200).send()
+}
 )
 
 
@@ -82,7 +82,7 @@ inicializaaws()
 app.use(bodyParser())
 app.use(inicializalogger())
 app.use(autenticador())
-app.use('/',router)
+app.use('/', router)
 app.use(inicializacatcher())
 app.listen(port, () => {
   logger.info(`SERVIDOR EM LISTEN - PORTA ${port}`)
